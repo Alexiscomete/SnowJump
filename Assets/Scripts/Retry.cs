4d8a2d5d959da72653cd94328f9ac65612421db9
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-08-30 18:46:05 +0200

    bug--

--- Assets/Scripts/Retry.cs
+++ Assets/Scripts/Retry.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Retry : MonoBehaviour
 {
     public void DoRetry()
     {
         SceneManager.LoadScene("Game");
+        Console.WriteLine(22);
     }
+
 }
